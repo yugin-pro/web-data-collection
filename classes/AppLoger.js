32fc114bf186f5744bd6bb113da1906ab14db8e0
Yugin Pro <yugin.pro@gmail.com>
2023-06-13 00:16:10 +0300

    uninst mongo

--- classes/AppLoger.js
+++ classes/AppLoger.js
@@ -1,7 +1,7 @@
 import { readFileSync } from 'node:fs'
 import { appendFile } from 'node:fs/promises'
 import { Buffer } from 'node:buffer'
-import { MongoClient } from 'mongodb'
+
 
 export default class AppLoger {
     constructor(path_to_config) {
@@ -13,9 +13,6 @@ export default class AppLoger {
     }
     async log(logData){
         return appendFile('./log/data.json',JSON.stringify(logData)+'\n')
-        // const client = new MongoClient(app.config.database.url);
-        // const database = client.db(app.config.database.name);
-        // const logCollection = database.collection(app.config.database.table);
     }
     get pixel() {
         return Buffer.from('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjuP2f4T8AB20C2vgNEPMAAAAASUVORK5CYII=','base64')
